7bc036f615b2d2428023d6fbe10d01f63086058e
Your Name <you@example.com>
2025-04-25 15:28:22 +0530

    9

---
+++ Record 9/Interface.java
@@ -0,0 +1,45 @@
+interface Student
+  {
+  void showAcademicScore();
+  }
+
+interface Sports
+  {
+  void showSportsScore();
+  }
+
+
+class Result implements Student, Sports 
+   {
+    private int academicScore = 76;  
+    private int sportsScore = 85;    
+ 
+    public void showAcademicScore() 
+        {
+        System.out.println("Academic Score: " + academicScore);
+        }
+
+   
+    public void showSportsScore() 
+        {
+        System.out.println("Sports Score: " + sportsScore);
+        }
+
+    
+    public void displayResult()
+        {
+        showAcademicScore();
+        showSportsScore();
+        }
+        }
+
+
+public class Interface 
+    {
+    public static void main(String[] args) 
+        {
+        Result studentResult = new Result();
+        studentResult.displayResult();
+        }
+    }
+
